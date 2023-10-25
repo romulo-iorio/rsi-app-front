d6354a70a0d2a7b2b2220b6f9ba2c7bb06e06490
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-25 19:46:14 -0300

    feat: creating login page

--- app/pages/Login/index.tsx
+++ app/pages/Login/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { InputWithLabel, Button } from "@/app/components";
 
 export const Login: React.FC = () => {
-  return <>Hello LogIn</>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-start w-full h-full gap-[2rem]">
+        <InputWithLabel label="E-mail" placeholder="Preencha seu e-mail" />
+
+        <InputWithLabel
+          label="Senha"
+          placeholder="Preencha sua senha"
+          password
+        />
+      </div>
+
+      <div className="absolute flex items-center justify-around w-[79%] h-[5rem] bottom-[3rem]">
+        <Button label="Recuperar senha" />
+
+        <Button label="Cadastro" />
+
+        <Button label="Entrar" />
+      </div>
+    </>
+  );
 };
