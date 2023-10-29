d9becb9bee187e36a5094fec262ecbeebda8b90f
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 16:29:59 -0300

    feat: adjusting responsivity on BaseContentContainer

--- app/components/BaseContentContainer.tsx
+++ app/components/BaseContentContainer.tsx
@@ -8,7 +8,7 @@ export const BaseContentContainer: React.FC<Props> = ({
   className,
 }) => (
   <div
-    className={`flex flex-col items-center justify-start w-full h-full gap-[2rem] pt-[3rem] px-[5rem] text-black ${className}`}
+    className={`flex flex-col items-center justify-start w-full h-full gap-[2rem] pt-[3rem] px-[0.5rem] md:px-[1rem] lg:px-[5rem] text-black ${className}`}
   >
     {children}
   </div>
