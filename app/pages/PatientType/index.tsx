40129c5e1970385e4de47c1f91e13ee03347837a
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-01 20:24:37 -0300

    feat: cleaning PatientType routings

--- app/pages/PatientType/index.tsx
+++ app/pages/PatientType/index.tsx
@@ -1,45 +1,28 @@
 import { BaseLayout, Button } from "@/app/components";
-import { PatientTypeEnum, usePageContext } from "@/app/store";
-import { useRoutes } from "@/app/hooks";
 
 const buttonsClassName = "!w-[20rem] !h-[7rem]";
 
+const emptyFunction = () => null;
+
 export const PatientType: React.FC = () => {
-  const {
-    goToNotICURespiratoryTractCheckUp1,
-    goToICURespiratoryTractCheckUp1,
-    goToLogin,
-  } = useRoutes();
-  const { setPatientType } = usePageContext();
-
-  const selectICU = () => {
-    setPatientType(PatientTypeEnum.ICU);
-    goToICURespiratoryTractCheckUp1();
-  };
-
-  const selectNotICU = () => {
-    setPatientType(PatientTypeEnum.NOT_ICU);
-    goToNotICURespiratoryTractCheckUp1();
-  };
-
   return (
     <BaseLayout.Root>
       <BaseLayout.Content className="!justify-around !h-[75%]">
         <Button
           className={buttonsClassName}
-          onClick={selectICU}
+          onClick={emptyFunction}
           label="Paciente UTI"
         />
 
         <Button
           className={buttonsClassName}
-          onClick={selectNotICU}
+          onClick={emptyFunction}
           label="Paciente Não UTI"
         />
       </BaseLayout.Content>
 
       <BaseLayout.Buttons>
-        <Button onClick={goToLogin} label="Voltar" />
+        <Button onClick={emptyFunction} label="Voltar" />
       </BaseLayout.Buttons>
     </BaseLayout.Root>
   );
