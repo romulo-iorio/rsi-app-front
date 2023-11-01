dd5e91672e91f592580e3ea1b04d5c207c29bac7
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-01 20:06:39 -0300

    feat: improving LogoOnClipboard, making it bigger on login screen and cover

--- app/components/PageLayout/LogoOnClipboard.tsx
+++ app/components/PageLayout/LogoOnClipboard.tsx
@@ -1,13 +1,52 @@
+import { usePageContext } from "@/app/store";
 import Image from "next/image";
 
-export const LogoOnClipboard = () => (
-  <Image
-    className="absolute left-1/2 transform -translate-x-1/2 top-[3rem]"
-    alt="Logo of RSI App"
-    src="/logo_full.svg"
-    objectFit="contain"
-    quality={100}
-    height={150}
-    width={150}
-  />
-);
+const useLogoSize = () => {
+  const { title } = usePageContext();
+  const isLoginPage = title === "Login";
+
+  if (isLoginPage) return 330;
+
+  return 220;
+};
+
+const useTopClass = () => {
+  const { title } = usePageContext();
+  const isCover = title === "Capa";
+  const isLoginPage = title === "Login";
+
+  if (isCover) return "top-[5rem]";
+
+  if (isLoginPage) return "top-[5rem]";
+
+  return "top-[3rem]";
+};
+
+interface Props {
+  show?: boolean;
+  size?: number;
+}
+
+export const LogoOnClipboard: React.FC<Props> = ({
+  size: parsedSize,
+  show,
+}) => {
+  const { title } = usePageContext();
+  const topClass = useTopClass();
+  const size = useLogoSize();
+
+  const isCover = title === "Capa";
+  if (isCover && !show) return null;
+
+  return (
+    <Image
+      className={`absolute left-1/2 transform -translate-x-1/2 ${topClass}`}
+      height={parsedSize ?? size}
+      width={parsedSize ?? size}
+      alt="Logo of RSI App"
+      src="/logo_full.svg"
+      objectFit="contain"
+      quality={100}
+    />
+  );
+};
