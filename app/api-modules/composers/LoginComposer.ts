51b26bf32984caec8c1c3d10163066ddadb46426
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 20:47:51 -0300

    feat: adjusting MongoDbService to be a class with a static method

--- app/api-modules/composers/LoginComposer.ts
+++ app/api-modules/composers/LoginComposer.ts
@@ -1,10 +1,15 @@
-import { CryptoService, JwtService, UserRepository, getDb } from "../infra";
+import {
+  MongoDbService,
+  UserRepository,
+  CryptoService,
+  JwtService,
+} from "../infra";
 import { LoginUseCase } from "../applications";
 
 export class LoginComposer {
   static async compose() {
     return new LoginUseCase(
-      new UserRepository(await getDb()),
+      new UserRepository(await MongoDbService.getDb()),
       new CryptoService(),
       new JwtService()
     );
--- app/api-modules/composers/RegistrationComposer.ts
+++ app/api-modules/composers/RegistrationComposer.ts
@@ -1,10 +1,10 @@
-import { CryptoService, UserRepository, getDb } from "../infra";
+import { CryptoService, UserRepository, MongoDbService } from "../infra";
 import { RegistrationUseCase } from "../applications";
 
 export class RegistrationComposer {
   static async compose() {
     return new RegistrationUseCase(
-      new UserRepository(await getDb()),
+      new UserRepository(await MongoDbService.getDb()),
       new CryptoService()
     );
   }
