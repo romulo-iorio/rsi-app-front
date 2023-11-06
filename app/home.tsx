09de894614c5c9bcf7ff4ca9f275aa2cfa3c1eb0
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 22:34:31 -0300

    feat: creating PageWrapper on home

--- app/home.tsx
+++ app/home.tsx
@@ -4,27 +4,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useMemo } from "react";
 
+import type { RouteProps } from "./routes";
 import { PageLayout } from "./components";
 import pages from "./routes";
 
 import "react-toastify/dist/ReactToastify.css";
 
-const Home = () => {
-  const Pages = useMemo(
-    () =>
-      pages.map(({ Component, path, name }) => (
-        <Route
-          path={path}
-          key={path}
-          element={
-            <PageLayout title={name}>
-              <Component />
-            </PageLayout>
-          }
-        />
-      )),
-    []
+const PageWrapper: React.FC<RouteProps> = ({
+  isPrivate,
+  Component,
+  name,
+  path,
+}) => {
+  return (
+    <Route
+      path={path}
+      key={path}
+      element={
+        <PageLayout title={name} isPrivate={isPrivate}>
+          <Component />
+        </PageLayout>
+      }
+    />
   );
+};
+
+const Home = () => {
+  const Pages = useMemo(() => pages.map(PageWrapper), []);
 
   return (
     <>
