7bcfa26597191f623d770d27255886dfe5a8cd4f
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 21:01:05 -0300

    feat: creating JwtService.verifyToken

--- app/api-modules/infra/services/JwtService.ts
+++ app/api-modules/infra/services/JwtService.ts
@@ -11,4 +11,8 @@ export class JwtService {
   generateToken(payload: JwtPayload): string {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
   }
+
+  verifyToken(token: string): JwtPayload {
+    return jwt.verify(token, JWT_SECRET) as JwtPayload;
+  }
 }
