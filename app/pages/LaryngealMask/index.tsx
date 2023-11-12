42d5ecc083c406fb9ad71e5d6f8ab339fd044aab
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:32:01 -0300

    feat: adjustments on LaryngealMask

--- app/pages/LaryngealMask/index.tsx
+++ app/pages/LaryngealMask/index.tsx
@@ -1,31 +1,50 @@
-import { toast } from "react-toastify";
+import { useState } from "react";
 
 import { BaseLayout, Button, FlowChartQuestion } from "@/app/components";
 import { useRoutes } from "@/app/hooks";
 
 export const optionsButtonsClassName =
-  "!md:lg:w-[24rem] !md:lg:h-[8rem] !w-[13rem] !h-[5rem] !text-[0.75rem]";
+  "!md:lg:w-[24rem] !md:lg:h-[8rem] !w-[13rem] !h-[5rem] !text-[0.75rem] opacity-[0.75]";
+
+const contentStyle = {
+  background: "url(/laryngeal-mask.png) no-repeat center center",
+  backgroundSize: "contain",
+  transform: "rotate(-45deg)",
+};
 
 export const LaryngealMask: React.FC = () => {
-  const { goBack, goToCervicalAccess } = useRoutes();
+  const { goToAfterAnesthesicInduction, goToCervicalAccess } = useRoutes();
+  const [showText, setShowText] = useState(false);
+
+  const onClick = () => setShowText(true);
 
   return (
     <BaseLayout.Root>
-      <BaseLayout.Content className="!justify-around !h-[75%]">
-        <p className="text-[1rem] lg:px-0 px-5">
+      <BaseLayout.Content className="!justify-between !h-[80%] relative">
+        <div
+          className="w-full h-full absolute top-0 left-0 z-[-1]"
+          style={contentStyle}
+        />
+
+        <p className="text-[1rem] bg-[#90C7BEAA] rounded-[1rem] p-2">
           3 tentativas com modelos/tamanhos diferentes​
         </p>
 
-        <FlowChartQuestion question="Resultado?" />
+        <FlowChartQuestion question="Resultado após máscara laríngea?" />
 
-        <div className="flex flex-row justify-center items-center gap-[0.5rem] lg:gap-[3rem]">
-          <Button
-            className={optionsButtonsClassName}
-            onClick={() => {
-              toast.success("Parar e pensar");
-            }}
-            label="Sucesso ou insucesso com boa ventilação por máscara"
-          />
+        <div
+          className="flex flex-row justify-center items-start gap-[1rem] lg:gap-[3rem]"
+          style={{ paddingBottom: showText ? "0" : "2rem" }}
+        >
+          <div className="flex flex-col justify-center items-center gap-[0.5rem]">
+            <Button
+              className={optionsButtonsClassName}
+              onClick={onClick}
+              label="Sucesso ou insucesso com boa ventilação por máscara"
+            />
+
+            {showText && <p>Parar e pensar</p>}
+          </div>
 
           <Button
             className={optionsButtonsClassName}
@@ -36,7 +55,7 @@ export const LaryngealMask: React.FC = () => {
       </BaseLayout.Content>
 
       <BaseLayout.Buttons>
-        <Button onClick={goBack} label="Voltar" />
+        <Button onClick={goToAfterAnesthesicInduction} label="Voltar" />
       </BaseLayout.Buttons>
     </BaseLayout.Root>
   );
