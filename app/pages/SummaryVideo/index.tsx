7ce030db955f0b76c1f816cdb045c2395e74ed63
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:34:07 -0300

    feat: adjusting button links on SummaryVideo

--- app/pages/SummaryVideo/index.tsx
+++ app/pages/SummaryVideo/index.tsx
@@ -2,19 +2,18 @@ import { BaseLayout, Button } from "@/app/components";
 import { useRoutes } from "@/app/hooks";
 
 export const SummaryVideo: React.FC = () => {
-  const { goToRapidSequenceInductionTips, goToWhatAboutAnesthetists } =
-    useRoutes();
+  const { goToCervicalAccessVideo, goToWhatAboutAnesthetists } = useRoutes();
 
   return (
     <BaseLayout.Root>
-      <BaseLayout.Content className="!justify-around !h-[80%] !pt-[0rem]">
+      <BaseLayout.Content className="!justify-around !h-[80%] !pt-[1rem]">
         <video className="!w-full !h-full" autoPlay controls>
           <source src="/pre-oxygenation.mp4" type="video/mp4" />
         </video>
       </BaseLayout.Content>
 
       <BaseLayout.Buttons>
-        <Button onClick={goToRapidSequenceInductionTips} label="Voltar" />
+        <Button onClick={goToCervicalAccessVideo} label="Voltar" />
 
         <Button onClick={goToWhatAboutAnesthetists} label="Próximo" />
       </BaseLayout.Buttons>
