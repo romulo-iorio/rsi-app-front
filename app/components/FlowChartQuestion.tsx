8beb556b118b45da74132afed12dbfa7b71d2f20
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:26:47 -0300

    feat: adding opacity to FlowChartQuestion

--- app/components/FlowChartQuestion.tsx
+++ app/components/FlowChartQuestion.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 export const FlowChartQuestion: React.FC<Props> = ({ question }) => (
-  <div className="flex flex-col h-[10rem] w-[25rem] items-center justify-center bg-[#90C7BE] rounded-[3rem]">
-    <p className="text-black font-bold text-[2rem]">{question}</p>
+  <div className="flex flex-col h-[10rem] w-[25rem] items-center justify-center bg-[#90C7BE77] rounded-[3rem]">
+    <p className="text-black font-bold text-[1.325rem]">{question}</p>
   </div>
 );
