b5a47a83fa2a743d066972a127306e4b9676dbfc
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:29:39 -0300

    refactor: extracting AwakeIntubation listItems to its own file

---
+++ app/pages/AwakeIntubation/listItems.tsx
@@ -0,0 +1,15 @@
+import { ListItemProps } from "../RapidSequenceInduction/interfaces";
+
+export const listItems: ListItemProps[] = [
+  { text: "O₂ nasal 5 L/min. Catéter de alto fluxo?" },
+  { text: "Atropina 0.25mg" },
+  {
+    text: "Remifentanil 1-3ng/ml ou midazolam 0.5mg/vez, dexmedetomidina 0.5μg/kg/h em 10'",
+  },
+  {
+    text: "Lidocaína spray em vias aéreas até 9mg/kg",
+  },
+  {
+    text: "Bloqueios como transcrico e laríngeo superior?",
+  },
+];
