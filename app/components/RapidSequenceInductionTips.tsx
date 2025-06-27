ed0d571332b87bd9f9a3c6501adb17b14c32acdd
Romulo Iorio <romuloiorio@hotmail.com>
2025-06-27 11:07:39 -0300

    feat: general adjustments

--- app/components/RapidSequenceInductionTips.tsx
+++ app/components/RapidSequenceInductionTips.tsx
@@ -7,14 +7,18 @@ export const RapidSequenceInductionTips: React.FC = () => {
     <div className="flex flex-col w-1/2 h-full justify-center">
       <div className="flex flex-col w-fit h-fit justify-center gap-[2rem] bg-red-300 rounded-3xl p-3 lg:p-3">
         <span>
-          <b>{t("Components.RapidSequenceInductionTips.ATTENTION")}</b>
-          <p>{t("Components.RapidSequenceInductionTips.REDUCE_DOSAGE")}</p>
-          <p>{t("Components.RapidSequenceInductionTips.HYPNOTICS_EFFECT")}</p>
+          <b>{t("Components.RapidSequenceInductionTips.Attention.Title")}</b>
+          <p>
+            {t("Components.RapidSequenceInductionTips.Attention.Content.1")}
+          </p>
+          <p>
+            {t("Components.RapidSequenceInductionTips.Attention.Content.2")}
+          </p>
         </span>
 
         <span>
           <b>{t("Components.RapidSequenceInductionTips.Caution.Title")}</b>
-          <p>{t("Components.RapidSequenceInductionTips.Caution.Content")}</p>
+          <p>{t("Components.RapidSequenceInductionTips.Caution.Content.1")}</p>
         </span>
       </div>
     </div>
--- app/pages/RapidSequenceInduction/TipsPopover.tsx
+++ app/pages/RapidSequenceInduction/TipsPopover.tsx
@@ -1,19 +1,24 @@
+import { useTranslation } from "react-i18next";
 import { BsLightbulb } from "react-icons/bs";
 
 import { RapidSequenceInductionTips } from "@/app/components/RapidSequenceInductionTips";
 import Popover from "@/app/components/Popover";
 
-export const TipsPopover: React.FC = () => (
-  <Popover placement="top">
-    <Popover.Trigger>
-      <button className="inline-flex gap-2 bg-yellow-200 rounded-full p-2 cursor-pointer hover:bg-yellow-300 active:transform active:translate-y-0.5">
-        <BsLightbulb size={20} />
-        <span>Dicas</span>
-      </button>
-    </Popover.Trigger>
+export const TipsPopover: React.FC = () => {
+  const { t } = useTranslation("common");
+  const title = t("TipsPopover.Title");
+  return (
+    <Popover placement="top">
+      <Popover.Trigger>
+        <button className="inline-flex gap-2 bg-yellow-200 rounded-full p-2 cursor-pointer hover:bg-yellow-300 active:transform active:translate-y-0.5">
+          <BsLightbulb size={20} />
+          <span>{title}</span>
+        </button>
+      </Popover.Trigger>
 
-    <Popover.Content>
-      <RapidSequenceInductionTips />
-    </Popover.Content>
-  </Popover>
-);
+      <Popover.Content>
+        <RapidSequenceInductionTips />
+      </Popover.Content>
+    </Popover>
+  );
+};
